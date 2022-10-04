ceea06514784f5b6d7689962b2fbc1dc45f5f5c8
Rafael Yuji Sato <sato0028@algonquinlive.com>
2022-10-04 11:22:05 -0400

    Page 1 incomplete

--- app/src/main/java/algonquin/cst2335/rafaelsandroidlabs/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rafaelsandroidlabs/MainActivity.java
@@ -2,18 +2,33 @@ package algonquin.cst2335.rafaelsandroidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+
+import algonquin.cst2335.rafaelsandroidlabs.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "MainActivity";
+    private ActivityMainBinding variableBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+
+        variableBinding.login.setOnClickListener( clk->
+        {
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+            startActivity(nextPage);
+        });
+
         setContentView(R.layout.activity_main);
-        Log.w( TAG, "In onCreate() - Loading Widgets" );
+        Log.w(TAG, "In onCreate() - Loading Widgets");
+    }
 
     @Override
     protected void onStart() {
