7ec512ea594622db945ca63f8a18dc8160739676
Rafael Yuji Sato <sato0028@algonquinlive.com>
2022-11-06 15:35:51 -0500

    Lab 6 part 1 complete

--- app/src/main/java/algonquin/cst2335/rafaelsandroidlabs/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/rafaelsandroidlabs/ChatRoom.java
@@ -17,13 +17,36 @@ import algonquin.cst2335.rafaelsandroidlabs.databinding.ActivityChatRoomBinding;
 public class ChatRoom extends AppCompatActivity {
     ActivityChatRoomBinding binding;
 
+    RecyclerView.Adapter<MyRowHolder> adapter;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());  
-        
-        binding.recycleView.setAdapter(adapter = new RecyclerView.Adapter<>())
+        setContentView(binding.getRoot());
+
+        binding.recycleView.setAdapter(adapter = new RecyclerView.Adapter<MyRowHolder>() {
+            @NonNull
+            @Override
+            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { //This function creates a ViewHolder object which represents a single row in the list
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) { //This initializes a ViewHolder to go at the row specified by the position parameter.
+            }
+
+            @Override
+            public int getItemCount() { //This function just returns an int specifying how many items to draw.
+                return 0;
+            }
+        });
     }
+    class MyRowHolder extends RecyclerView.ViewHolder {
+        public MyRowHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 
 }
--- app/src/main/java/algonquin/cst2335/rafaelsandroidlabs/data/MainViewModel.java
+++
@@ -1,9 +0,0 @@
-package algonquin.cst2335.rafaelsandroidlabs.data;
-
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel { //survive rotation changes
-    public MutableLiveData<String> editString = new MutableLiveData<>();
-    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
-}
