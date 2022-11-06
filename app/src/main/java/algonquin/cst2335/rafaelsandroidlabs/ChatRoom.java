e0321ec345b7d011f9954e7b04e492169aacb6a3
Rafael Yuji Sato <sato0028@algonquinlive.com>
2022-11-06 16:28:46 -0500

    Lab 6 part 2 incomplete

--- app/src/main/java/algonquin/cst2335/rafaelsandroidlabs/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/rafaelsandroidlabs/ChatRoom.java
@@ -10,11 +10,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 import algonquin.cst2335.rafaelsandroidlabs.databinding.ActivityChatRoomBinding;
 
 public class ChatRoom extends AppCompatActivity {
+
+    ArrayList<String> messages = new ArrayList<>();
+
     ActivityChatRoomBinding binding;
 
     RecyclerView.Adapter<MyRowHolder> adapter;
@@ -25,26 +30,51 @@ public class ChatRoom extends AppCompatActivity {
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
+
+        binding.button2.setOnClickListener( click ->{
+            messages.add(binding.textInput.getText().toString());
+
+            //clear the previous text:
+            adapter.notifyItemChanged(messages.size()-1); //which position has changed
+            binding.textInput.setText(""); //remove what was there
+        });
+
         binding.recycleView.setAdapter(adapter = new RecyclerView.Adapter<MyRowHolder>() {
             @NonNull
             @Override
             public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { //This function creates a ViewHolder object which represents a single row in the list
-                return null;
+                View root;
+                root = getLayoutInflater().inflate(R.layout.sent_message, parent, false);
+                return new MyRowHolder(root);
             }
 
             @Override
             public void onBindViewHolder(@NonNull MyRowHolder holder, int position) { //This initializes a ViewHolder to go at the row specified by the position parameter.
+                String thisRow = messages.get(position);
+
+                holder.message.setText(thisRow);
+                holder.time.setText("");
             }
 
             @Override
             public int getItemCount() { //This function just returns an int specifying how many items to draw.
+                return messages.size();
+            }
+
+            @Override
+            getItemViewType(int position){
                 return 0;
             }
         });
     }
     class MyRowHolder extends RecyclerView.ViewHolder {
+        TextView message;
+        TextView time;
         public MyRowHolder(@NonNull View itemView) {
             super(itemView);
+            message = itemView.findViewById(R.id.message);
+            time = itemView.findViewById(R.id.time);
         }
     }
 
