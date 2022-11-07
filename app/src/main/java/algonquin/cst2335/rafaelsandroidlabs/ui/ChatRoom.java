df603ce4efe4c740bbea5951938cb447cad9852d
Rafael Yuji Sato <sato0028@algonquinlive.com>
2022-11-07 10:04:37 -0500

    Lab 6 part 3 incomplete_2

--- app/src/main/java/algonquin/cst2335/rafaelsandroidlabs/ui/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/rafaelsandroidlabs/ui/ChatRoom.java
@@ -8,7 +8,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
